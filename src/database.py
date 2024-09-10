e6bbc641ffb7a20f7703dd83a86cf167a3a08d0a
Adeeb Mirza <devilmirzaa3@gmail.com>
2024-09-11 01:05:46 +0530

    My Second Commit

---
+++ src/database.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from src.exception import CustomException
+from src.logger import logger
+load_dotenv()
+
+try:
+    client = MongoClient("mongodb://localhost:27017")
+    db = client['IntelliHelper']
+    users_collection = db['users']
+except Exception as e:
+    raise CustomException(e,sys)
+
+
+def create_user(user_data):
+    logger.info("Creating user")
+    try:
+        users_collection.insert_one(user_data)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def find_user(login_input):
+    logger.info("Finding user")
+    try:
+        return users_collection.find_one({"$or": [{"username": login_input}, {"email": login_input}]})
+    except Exception as e:
+        raise CustomException(e,sys)
---
+++ src/profile/form.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, EqualTo
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from src.logger import logger
+
+
+#users = {}
+
+class SignupForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired(), Length(min=4, max=30)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Signup')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=30)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
+    submit = SubmitField('Login')

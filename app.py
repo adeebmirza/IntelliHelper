4749b4ff1eaa11f0d5deccf5c433163fc6bf3aaa
Adeeb Mirza <devilmirzaa3@gmail.com>
2024-10-11 23:43:09 +0530

    fix

--- app.py
+++ app.py
@@ -1,29 +1,4 @@
-'''
 # make sure to download python -m spacy download en_core_web_sm
-from src.profile.verification_email import send, send_forget
-from flask import Flask, flash, render_template, request, redirect, url_for,session
-from src.profile.form import SignupForm, LoginForm, ResetForm
-from src.ATS.resume_score import parse_resume, calculate_ats_score
-from src.database import create_user, find_user, get_user_by_id, users_collection
-from passlib.hash import argon2
-from src.database import users_collection
-from argon2 import PasswordHasher
-import pyotp
-from src.profile.forget import generate_reset_token, verify_reset_token
-argon2 = PasswordHasher()
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'a1b2c3d4e5f67890abcdef1234567890abcdef1234567890abcdef12345678'
-
-otp_secrets = {}
-
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-'''
 
 from flask import Flask
 from src.routes.auth import auth_bp
